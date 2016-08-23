167b93ec7dc0badea3d3a0411cb26c69b41591e2
Xu Lei <a871460434@gmail.com>
2016-08-23 15:23:00 +0800

    add function to handle POST request

---
+++ models/user.js
@@ -0,0 +1,58 @@
+var mongo = require('.db');
+function User(user) {
+    this.name = user.name;
+    this.password = user.password;
+    this.email = user.email;
+};
+
+module.exports = User;
+
+User.prototype.save = function(callback){
+    var user = {
+        name: this.name,
+        password: this.password,
+        email:this.email
+    };
+    mongo.open(function(err,db){
+        if (err){
+            return callback(err);
+        }
+        db.connection('user',function(err,collection){
+            if (err) {
+                mongo.close();
+                return callback(err);
+            }
+            collection.insert(user,{
+                safe:true
+            },function(err,user){
+                mongo.close();
+                if (err) {
+                    return callback(err);
+                }
+                callback(null,user[0]);
+            });
+        });
+    });
+}
+User.get = function (name,callback){
+    mongo.open(function(err,db){
+        if (err) {
+            return callback(err);
+        }
+        db.collection('user',function (err,collection){
+            if (err) {
+                mongo.close();
+                return callback(err);
+            }
+            collection.findOne({
+                name:name
+            },function(err,user){
+                mongo.close();
+                if (err) {
+                    return callback(err);
+                }
+                callback(null,user);
+            });
+        });
+    });
+};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
-
+var crypto = require('crypto');
+var User = require('../models/user');
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
@@ -9,7 +10,40 @@ router.get('/reg', function (req, res) {
   res.render('reg', { title: 'register' })
 });
 router.post('/reg', function (req, res) {
+  var name = req.body.name,
+      password = req.body.password,
+      password_repeat = req.body['password-repeat'];
+      if (password_repeat != password){
+        req.flash('error','password is not the same');
+        return res.redirect('/reg');
+      }
+      var md5 = crypto.createHash('md5'),
+          password = md5.update(req.body.password).digest('hex');
+          var newUser = new User({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email
+          });
+      User.get(newUser.name, function(err,user){
+        if (err) {
+          req.flash('error',err);
+          return res.redirect('/');
+        }
+        if (user) {
+          req.frash('error','User existed');
+          return res.redirect('/reg');
+        }
+        newUser.save(function(err,user){
+          if (err){
+            req.flash('error',error);
+            return res.redirect('/reg');
+          }
+          req.session.user = user;
+          req.flash('success','register success');
+          res.redirect('/');
 
+        });
+      });
 });
 router.get('/login',function(req,res){
   res.render('login',{title:'Login'});
