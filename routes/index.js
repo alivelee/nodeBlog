f5dd686afccc9923de6a8580e531d5304d5cd36a
Xu Lei <a871460434@gmail.com>
2016-08-24 17:52:14 +0800

    add checklogin function

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,23 @@ var crypto = require('crypto');
 var User = require('../models/user');
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { 
+    title:'Main Page',
+    user: req.session.user,
+    success: req.flash('success').toString(),
+    error: req.flash('error').toString()
+ });
 });
+router.get('/reg',checkNotLogin);
 router.get('/reg', function (req, res) {
-  res.render('reg', { title: 'register' })
+  res.render('reg', { 
+    title: 'register',
+    user: req.session.user,
+    success: req.flash('success').toString(),
+    error: req.flash('error').toString()
+  });
 });
+router.post('/reg',checkNotLogin);
 router.post('/reg', function (req, res) {
   var name = req.body.name,
       password = req.body.password,
@@ -45,8 +57,66 @@ router.post('/reg', function (req, res) {
         });
       });
 });
+router.get('/login',checkNotLogin);
 router.get('/login',function(req,res){
-  res.render('login',{title:'Login'});
+  res.render('login',{
+    title:'Login',
+    user: req.session.user,
+    success: req.flash('success').toString(),
+    error: req.flash('error').toString()
+  });
 });
+router.post('/login',checkNotLogin);
+router.post('/login',function(req,res){
+  var md5 = crypto.createHash('md5'),
+      password = md5.update(req.body.password).digest('hex');
+  User.get(req.body.name,function(err,user){
+      if(!user){
+        req.flash('error','user not exist');
+        return res.redirect('/login');
+      }
+      if (user.password != password){
+        req.flash('error','password incorrect');
+        return res.redirect('/login');
+      }
+      req.session.user = user;
+      req.flash('success','login success');
+      res.redirect('/');
+  });
+});
+router.get('/logout',function(req,res){
+  req.session.user = null;
+  req.flash('success','logout success');
+  res.redirect('/');
+});
+router.get('/post',checkLogin);
+router.get('/post',function(req,res){
+  res.render('post',{
+    title:'Post',
+    user:req.session.user,
+    succuess:req.flash('success').toString,
+    error:req.flash('error').toString
+  });
+});
+router.post('/post',checkLogin);
+router.post('/post',function(req,res){
+  
+});
+function checkLogin(req,res,next){
+  if (!req.session.user){
+    req.flash('error','Not Login');
+    res.redirect('/login');
+  }
+  next();
+}
+function checkNotLogin(req,res,next){
+  if (req.session.user){
+    req.flash('error','Logined');
+    res.redirect('back');
+  }
+  next();
+}
+
+
 module.exports = router;
  
