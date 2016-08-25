7d774e60c7f9f1d57ccd2f5af68508f818085091
Xu Lei <a871460434@gmail.com>
2016-08-25 13:22:48 +0800

    add markdown support

--- models/post.js
+++ models/post.js
@@ -1,4 +1,5 @@
 var mongo = require('./db');
+var markdown = require('markdown').markdown;
 function Post(name,title,post){
     this.name = name;
     this.title = title;
@@ -13,7 +14,7 @@ Post.prototype.save = function(callback){
         year:date.getFullYear(),
         month:date.getFullYear() + "-" + (date.getMonth() + 1),
         day:date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
-        minute:date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "" + date.getHours() + "-" + (date.getMinutes() < 10 ?'0' + date.getMinutes():date.getMinutes())
+        minute:date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + "-" + (date.getMinutes() < 10 ?'0' + date.getMinutes():date.getMinutes())
     }
     var post = {
         name:this.name,
@@ -57,12 +58,15 @@ Post.get = function(name,callback){
               query.name = name;
           }
           collection.find(query).sort({
-              time:-1
+              time:-1 
           }).toArray(function(err,docs){
               mongo.close();
               if (err){
                 return callback(err);
               }
+              docs.forEach(function(doc){
+                  doc.post = markdown.toHTML(doc.post);
+              });
               callback(null,docs);
           });
         });
