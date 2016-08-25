57bc315439757439ce62d9be01f6d7f30c6a3d15
Xu Lei <a871460434@gmail.com>
2016-08-25 16:00:21 +0800

    fixed time

--- models/post.js
+++ models/post.js
@@ -14,7 +14,7 @@ Post.prototype.save = function(callback){
         year:date.getFullYear(),
         month:date.getFullYear() + "-" + (date.getMonth() + 1),
         day:date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
-        minute:date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + "-" + (date.getMinutes() < 10 ?'0' + date.getMinutes():date.getMinutes())
+        minute:date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + (date.getMinutes() < 10 ?'0' + date.getMinutes():date.getMinutes())
     }
     var post = {
         name:this.name,
