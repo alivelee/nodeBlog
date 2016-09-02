aa0a658d0b9e987325cd014f71b39265ffe0c7c7
Xu Lei <a871460434@gmail.com>
2016-09-02 15:19:33 +0800

    add get archieve function

--- models/post.js
+++ models/post.js
@@ -178,3 +178,29 @@ Post.remove = function (name,day,title,callback){
         });
     });
 }
+Post.getArchieve = function(callback){
+    mongo.open(function(err,db){
+        if (err) {
+            return callback(err);
+        }
+        db.collection('post',function(err,collection){
+            if (err) {
+                mongo.close();
+                return callback(err);
+            }
+            collection.find({},{
+                "name":1,
+                "time":1,
+                "title":1
+            }).sort({
+                time:-1
+            }).toArray(function(err,docs){
+                mongo.close();
+                if (err){
+                    return callback(err);
+                }
+                callback(null,docs);
+            });
+        });
+    });
+}
